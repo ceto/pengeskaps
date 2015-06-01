3c9905e12bc63c9c9f7bc213abc8cd208097cf05
Gabor Szabo <szabogabi@gmail.com>
2015-06-01 21:09:24 +0200

    nagystart

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
+jQuery(document).ready(function() {
+	
 
+	/************* Main header Fixing ***********/
+  var htop = $('.navbar').offset().top - parseFloat($('.navbar').css('marginTop').replace(/auto/, 0));
+  $(window).scroll(function (event) {
+    var y = $(this).scrollTop();
+    if (y >= htop) {
+      $('.navbar').addClass('fixedhead');
+    } else {
+      $('.navbar').removeClass('fixedhead');
+    }
+    //$('body').attr('data-offset' ,  $('.banner').height() );
+  });
+
+});
